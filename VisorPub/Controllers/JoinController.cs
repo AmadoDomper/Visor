55bb48eb5881fd5e1e67847eafad3585c416d41a
AmadoDomper <amado.domper@gmail.com>
2019-12-02 22:21:22 -0500

    Add validation when creating new accounts

--- VisorPub/Controllers/JoinController.cs
+++ VisorPub/Controllers/JoinController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using EPostgres;
+using LNPostgres;
 
 namespace VisorPub.Controllers
 {
@@ -13,5 +15,53 @@ namespace VisorPub.Controllers
         {
             return View();
         }
+
+
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public JsonResult SaveAccount(Usuario oUser)
+        {
+            int resultado;
+            try
+            {
+                if (ValidationOK(oUser)) { 
+                    UsuarioLN oUsuarioLN = new UsuarioLN();
+
+                    resultado = oUsuarioLN.RegistrarModificarUsuario(oUser);
+                }
+                else
+                {
+                    //There is something wrong
+                    resultado = -10;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return Json(resultado, JsonRequestBehavior.AllowGet);
+        }
+
+        public bool ValidationOK(Usuario oUser)
+        {
+            if (!ValidateRequired(oUser)) return false;
+
+            return true;
+        }
+
+
+        public bool ValidateRequired(Usuario oUser)
+        {
+
+            if (oUser.cNombres == "" || oUser.cApellidoMa == "" || oUser.cApellidoPa == "" || oUser.cInstitucion == "" || oUser.cEmail == "" || oUser.cContrasena == "")
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+
     }
 }
